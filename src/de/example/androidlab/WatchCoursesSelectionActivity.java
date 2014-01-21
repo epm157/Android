35faba88f666cde68d8bff764a4e245e5e84d457
unknown <clear>
2014-01-21 19:40:06 +0100

    Course selecting for auto sync

--- src/de/example/androidlab/WatchCoursesSelectionActivity.java
+++ src/de/example/androidlab/WatchCoursesSelectionActivity.java
@@ -9,11 +9,14 @@ import roboguice.util.Ln;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -29,9 +32,10 @@ public class WatchCoursesSelectionActivity extends BaseActivity {
 
 	RoomArrayAdapter adapter;
 	List<Course> l2pRoomslist;
-	
 	Set<String> watchedCoursesIds;
-	
+	SharedPreferences app_preferences;
+	SharedPreferences.Editor editor;
+	Button submitWatchList;
 	//@InjectView(R.id.watched_course_selection_list)
 	private ListView listView;
 	
@@ -41,39 +45,54 @@ public class WatchCoursesSelectionActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		setContentView(R.layout.watched_course_selection);
 		
 		Intent x = this.getIntent();
         Bundle b=this.getIntent().getExtras();
 		ArrayList<Parcelable> rooms=b.getParcelableArrayList("rooms");
 		l2pRoomslist=new ArrayList<Course>();
+		l2pRoomslist.clear();
     	for(int i=0;i<rooms.size();i++)
     	{
     		Course lm=(Course)rooms.get(i);
     		l2pRoomslist.add(lm);
     	}
+    	watchedCoursesIds=new HashSet<String>();
+    	 app_preferences =  PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+    	 editor= app_preferences.edit();
+    	 if(app_preferences.getStringSet("courses", null) != null)
+    	 {
+    		 watchedCoursesIds=app_preferences.getStringSet("courses", null);
+    	 }
+    		 
+    	 if(watchedCoursesIds == null)
+    	 {
+    		watchedCoursesIds=new HashSet<String>();
+ 	    	Toast.makeText(getApplicationContext(),"No course for auto sync", Toast.LENGTH_SHORT).show();
+    	 }
+    	 else
+    	 {
+    		 Toast.makeText(getApplicationContext(),"Number of courses for syncing:  "+watchedCoursesIds.size(), Toast.LENGTH_SHORT).show();
+    	 }
+    	 
 		
-		
-		
-		setContentView(R.layout.watched_course_selection);
-		listView = (ListView) findViewById(R.id.watched_course_selection_list);
-		submitButton = (Button) findViewById(R.id.submit_watchList);
-		//List<Course> tempAll = new ArrayList<Course>();
-		
-		
-		
-		//loadCourses();
-		//tempAll=getAppService().l2pService_allCourses();
-		//Toast.makeText(getApplicationContext(),String.valueOf(tempAll.size()), Toast.LENGTH_SHORT).show();
-		 
+		listView = (ListView) findViewById(R.id.watched_course_selection_list); 
 		 adapter = new RoomArrayAdapter(getApplicationContext(), R.layout.room_list_item_checkbox, l2pRoomslist);
-	        //listView.setAdapter(adapter);
 	        
 		if(listView == null ) Ln.d("it is null"); else Ln.d("it is NOT null");
 		listView.setAdapter(adapter);
 		
+		
+		submitWatchList=(Button)findViewById(R.id.submit_watchList);
+   	 	submitWatchList.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				editor.clear();
+				editor.putStringSet("courses", watchedCoursesIds);
+				editor.commit();
+				finish();
+			}
+		});
 	}
-
 
 	private class RoomArrayAdapter extends ArrayAdapter<Course> 
     {
@@ -105,42 +124,28 @@ public class WatchCoursesSelectionActivity extends BaseActivity {
 			String str = ((Course)objects.get(position)).getTitle();
 			tw1.setText(str);
 			CheckBox cb = (CheckBox) row.findViewById(R.id.courseCheckBox);
-			cb.setChecked(true);
+			final String Id=((Course)objects.get(position)).getId();
+			if(watchedCoursesIds != null)
+				if(watchedCoursesIds.contains(Id))
+					cb.setChecked(true);
+				else
+					cb.setChecked(false);
+			cb.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+				@Override
+				public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+					 //Toast.makeText(getApplicationContext(),Id, Toast.LENGTH_SHORT).show();
+					
+					if(isChecked)
+						 watchedCoursesIds.add(Id);
+					 else
+						 watchedCoursesIds.remove(Id);
+					 
+					 Toast.makeText(getApplicationContext(),String.valueOf(watchedCoursesIds.size()+" "+Id), Toast.LENGTH_SHORT).show();
+				
+				}
+			});
 			return row;
 		}
     }
-	
-	
-	
-	public void loadCourses()
-	{
-		
-		AsyncTask<Void, Void, List<Course>> task = new AsyncTask<Void, Void, List<Course>>(){
-			ProgressDialog pd;
-			@Override
-			protected void onPreExecute() {
-				super.onPreExecute();
-				pd = ProgressDialog.show(WatchCoursesSelectionActivity.this, "Please Wait", "Getting List Of Courses");
-			}
-			
-			@Override
-			protected List<Course> doInBackground(Void... params) {
-				return getAppService().l2pService_allCourses();
-			}
-			
-			
-			@Override
-			protected void onPostExecute(List<Course> result) {
-				super.onPostExecute(result);
-				pd.dismiss();
-				//Toast.makeText(getApplicationContext(),String.valueOf(result.size()), Toast.LENGTH_SHORT).show();
-					
-			}
-		};
-		task.execute();
-		
-	}
-	
-    
 	
 }
